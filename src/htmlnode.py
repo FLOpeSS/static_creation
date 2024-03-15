98fa91f932a1b8d6fec936fc0b142a967aa91846
FLOpeSS <sneto4413@gmail.com>
2024-03-15 08:28:11 -0300

    Implementing leafnode and transforming to html text

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,12 +13,13 @@ class HTMLNode():
     raise NotImplementedError()
 
   def props_to_html(self):
-    # html_attr = ''.join([f' {key}="{value}"' if i > 0 else f'{key}="{value}"' for i, (key, value) in enumerate(self.props.items())])
      if self.props is None:
        return ""
+
      props_html = ""
      for prop in self.props:
        props_html += f' {prop}="{self.props[prop]}"'
+
      return props_html
 
 
@@ -27,4 +28,15 @@ class LeafNode(HTMLNode):
   def __init__(self, tag=None, value=None, children=None, props=None):
     super().__init__(tag, value, children, props)
 
+  def to_html(self)-> str:
+    if self.value == None or self.value == "":
+      raise ValueError
+
+    if self.tag == None or self.tag == "":
+      return str(self.value)
+
+    if bool(self.props) != False:
+      return f'<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>'
+
+    return f'<{self.tag}>{self.value}</{self.tag}>'
 
