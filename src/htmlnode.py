f1c16f398d009488faf40e6d76367babbb7ec5b9
FLOpeSS <sneto4413@gmail.com>
2024-03-19 22:24:42 -0300

    Adjusting ParenteNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,4 @@
+
 class HTMLNode():
   def __init__(self, tag=None, value=None, children=None, props=None):
     self.tag = tag
@@ -22,8 +23,6 @@ class HTMLNode():
 
      return props_html
 
-
-
 class LeafNode(HTMLNode):
   def __init__(self, tag=None, value=None, children=None, props=None):
     super().__init__(tag, value, children, props)
@@ -31,18 +30,32 @@ class LeafNode(HTMLNode):
   def __repr__(self):
     return f'Tag: {self.tag}, Value: {self.value}, Children: {self.children}, Props: {self.props}'
 
-
   def to_html(self)-> str:
-    if self.value == None or self.value == "":
-      raise ValueError
-
-    if self.tag == None or self.tag == "":
-      return str(self.value)
-
-    if bool(self.props) != False:
+    if self.value is None:
+      raise ValueError("Invalid HTML: invalid html")
+    if self.tag is None:
+      return self.value
+    if self.props:
       return f'<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>'
 
     return f'<{self.tag}>{self.value}</{self.tag}>'
+
+class ParentNode(HTMLNode):
+  def __init__(self, tag=None, children=None, props=None):
+    super().__init__(tag, children=children, props=props)
+
+  def __repr__(self):
+    return f'Tag: {self.tag}, Children: {self.children}, Props: {self.props}'
+
+  def to_html(self):
+    if self.tag is None:
+        raise ValueError("Invalid HTML: no tag")
+    if self.children is None:
+        raise ValueError("Invalid HTML: no children")
+    children = ''.join(child.to_html() for child in self.children)
+    return f'<{self.tag}>{children}</{self.tag}>'
+
+
 
 
 
